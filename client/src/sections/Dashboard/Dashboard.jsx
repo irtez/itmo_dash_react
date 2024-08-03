e51a4df0741b834e919c186b5db39ef2ffec74ec
irtez <lokipolik32@mail.ru>
2024-08-04 01:52:56 +0300

    added red line at 205

--- client/src/sections/Dashboard/Dashboard.jsx
+++ client/src/sections/Dashboard/Dashboard.jsx
@@ -75,7 +75,7 @@ const Table = ({table}) => {
               </thead>
               <tbody>
                   {rating_table.map((chel, index) => (
-                      <tr key={index} color={(chel.index >= 206) ? ('red') : ('green')}>
+                      <tr key={index} style={{borderBottom: (chel.index === 205) ? ('2px solid red') : ('')}} > 
                           <td>{chel.index}</td>
                           <td dangerouslySetInnerHTML={{__html: formatID(chel.id)}}></td>
                           <td>{chel.max_score}</td>
